da1f3bd2ba169cacb0b3d1744929569b7c53ede7
austintude <daniel@austintatiousdesign.co>
2020-09-15 17:33:11 -0500

    v2.0.5 mobile v desktop customizer options

--- header-custom.php
+++ header-custom.php
@@ -36,65 +36,59 @@ $tagline			= get_field('tagline');
 </head>
 
 <body <?php body_class(); ?>>
+<?php $analyticsloop = new \WP_Query( array( 'post_type' => 'analytics', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
 
+<?php while( $analyticsloop->have_posts() ) : $analyticsloop->the_post();
+$google_tag_manager_id	= get_field('google_tag_manager_id');
+$facebook_pixel_id	= get_field('facebook_pixel_id');
+$facebook_pixel_triggers	= get_field('facebook_pixel_triggers');
+?>
+<!-- Google Tag Manager -->
+<amp-analytics config="https://www.googletagmanager.com/amp.json?id=<?php echo $google_tag_manager_id; ?>&gtm.url=SOURCE_URL" data-credentials="include"></amp-analytics>
+<?php if ($facebook_pixel_id != null): ?>
 <!-- Begin "Facebook Pixel for AMP" || Help center -->
 <!-- Insert in Settings->HTML/CSS->Body -->
-<!-- IN:20200120-09-3 -->
-<amp-analytics type="facebookpixel" id="facebook-pixel">
+<amp-analytics type="facebookpixel" id="facebook-pixel" class="i-amphtml-layout-fixed i-amphtml-layout-size-defined" style="width:1px;height:1px;" i-amphtml-layout="fixed">
 <script type="application/json">
 {
     "vars": {
-        "pixelId": "570168077230923"
+        "pixelId": "<?php echo $facebook_pixel_id; ?>"
     },
     "triggers": {
         "trackPageview": {
             "on": "visible",
             "request": "pageview"
         },
-        "formSubmit": {
-            "on": "amp-form-submit-success",
+		<?php while (have_rows('facebook_pixel_triggers')) : the_row();
+
+// vars
+$trigger_title = get_sub_field('trigger_title');
+$trigger_selector = get_sub_field('trigger_selector');
+$trigger_on = get_sub_field('trigger_on');
+$trigger_event_name = get_sub_field('trigger_event_name');
+?>
+        "<?php echo $trigger_title; ?>": {
+			<?php if ($trigger_selector != null): ?>
+			"selector":"<?php echo $trigger_selector; ?>",
+				<?php endif; ?>
+            "on": "<?php echo $trigger_on; ?>",
             "request": "event",
             "vars": {
-            "eventName": "form-submission"
+            "eventName": "<?php echo $trigger_event_name; ?>"
             }
         },
-        "buttonClick1": {
-        "selector":".ctaButton",
-            "on": "click",
-            "request": "event",
-            "vars": {
-            "eventName": "Learn More X button click"
-            }
-		},
-		"buttonClick2": {
-        "selector":".asideListItem",
-            "on": "click",
-            "request": "event",
-            "vars": {
-            "eventName": "List Item in Aside ServicesTestimonialsPortfolioFindUs button click"
-            }
-        },
-		"buttonClick3": {
-        "selector":".btn-lg",
-            "on": "click",
-            "request": "event",
-            "vars": {
-            "eventName": "lets do this button click"
-            }
-        },
-		"buttonClick4": {
-        "selector":".amp-wp-enforced-sizes",
-            "on": "click",
-            "request": "event",
-            "vars": {
-            "eventName": "Footer image click"
-            }
+		<?php endwhile; ?>
+
+		"emptyForScriptCompliance": {
+
         }
     }
 }
 </script>
 </amp-analytics>
 <!-- End "Facebook Pixel for AMP" || Help center -->
+<?php endif; ?>
+<?php endwhile; wp_reset_query(); ?>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'wp-rig' ); ?></a>
