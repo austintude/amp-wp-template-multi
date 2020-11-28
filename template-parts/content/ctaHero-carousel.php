d9e125d754984c6f14f094f9d600778764d227dd
austintude <daniel@austintatiousdesign.co>
2020-11-28 14:53:49 -0600

    v2.5.6 minor layout issues resolved

---
+++ template-parts/content/ctaHero-carousel.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Template part for ctaHero
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+?>
+
+<?php $ctalinksloop2 = new \WP_Query( array( 'post_type' => 'cta_links', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
+
+<?php while( $ctalinksloop2->have_posts() ) : $ctalinksloop2->the_post();
+$cta_in_hero	= get_field('cta_in_hero');
+?>
+<?php  if( $cta_in_hero = 1 ): {
+	$cta_hero_x = 1;
+}
+endif;
+?>
+
+<?php endwhile; wp_reset_query(); ?>
+<?php  if( $cta_hero_x >= 1 ): { ?>
+<div class="heroButtons <?php echo get_theme_mod( 'hero_cta_grid_area', '' ); ?>Hero">
+<?php }
+else : ?>
+<div class="oneHeroButton">
+	<?php
+endif;
+?>
+<?php $ctalinksloop = new \WP_Query( array( 'post_type' => 'cta_links', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
+
+<?php while( $ctalinksloop->have_posts() ) : $ctalinksloop->the_post();
+$cta_text_color = '#fff';
+$ctahero_cta_text	= get_field('ctahero_cta_text');
+$ctahero_cta_url	= get_field('ctahero_cta_url');
+$cta_loading_image	= get_field('cta_loading_image');
+$cta_in_hero	= get_field('cta_in_hero');
+$cta_color	= get_field('cta_color');
+$cta_text_color	= get_field('cta_text_color');
+$cta_unique_id	= get_field('cta_unique_id');
+$cta_lightbox_toggle	= get_field('cta_lightbox_toggle');
+$cta_count = 0;
+?>
+<?php  if( $cta_in_hero != '0' ): { ?>
+
+	<div class="ctaHero">
+<div class="ctaButtonLink">
+
+<?php  if( $cta_lightbox_toggle != 0 ): { ?>
+			<button class="btn btn-lg btn-danger" on="tap:my-lightbox<?php echo $cta_unique_id; ?>" role="button" tabindex="0" style="background:<?php echo $cta_color; ?>; color:<?php echo $cta_text_color; ?>;"><?php echo $ctahero_cta_text; ?> »</button>
+			<amp-lightbox id="my-lightbox<?php echo $cta_unique_id; ?>" layout="nodisplay">
+    <div class="lightbox" on="tap:my-lightbox<?php echo $cta_unique_id; ?>.close" role="button" tabindex="0">
+
+      <amp-iframe width="350" height="600" layout="fixed"
+              sandbox="allow-scripts allow-same-origin allow-popups" frameborder="0"
+              src="<?php echo $ctahero_cta_url; ?>">
+    <amp-img layout="fill"
+             src="<?php echo $cta_loading_image['url']; ?>"
+             placeholder></amp-img>
+  </amp-iframe>
+    </div>
+  </amp-lightbox>
+  <?php }
+else : ?>
+			<button class="btn btn-lg btn-danger noLightBox" role="button" tabindex="0" style="background:<?php echo $cta_color; ?>; color:<?php echo $cta_text_color; ?>;" ><a href="<?php echo $ctahero_cta_url; ?>" style="color:<?php echo $cta_text_color; ?>;"><?php echo $ctahero_cta_text; ?> »</a></button>
+<?php
+endif;
+?>
+		</div><!-- ctaButtonLink -->
+</div> <!-- ctaHero -->
+		<?php
+}
+endif;
+?>
+		<?php endwhile; wp_reset_query(); ?>
+</div><!-- close oneHeroButton / heroButtons -->
+
+
--- template-parts/header/hero-amp-carousel-hero-text.php
+++ template-parts/header/hero-amp-carousel-hero-text.php
@@ -57,8 +57,8 @@ else : ?>
 endif;
 ?>
 
-		</div><!-- ctaButtonLink -->
-</div> <!-- ctaHero -->
+		</div>
+</div>
 </div>
 		<?php
 }
