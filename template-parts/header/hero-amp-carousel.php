3c9c0e77b8aa63e2fb4113bf98ebe74296c6e763
austintude <daniel@austintatiousdesign.co>
2021-02-11 16:04:35 -0600

    v2.7.7.2 added srcset functionality to amp-carousel images for LCP lift

--- template-parts/header/hero-amp-carousel.php
+++ template-parts/header/hero-amp-carousel.php
@@ -11,7 +11,35 @@
 // vars
 $hero_carousel_image = get_field('hero_carousel_image');
 ?>
-  <div><img data-hero layout="intrinsic" src="<?php echo $hero_carousel_image['url']; ?>" alt="<?php echo $hero_carousel_image['alt']; ?>"/>
+<?php
+
+/**
+ * Responsive Image Helper Function
+ *
+ * @param string $image_id the id of the image (from ACF or similar)
+ * @param string $image_size the size of the thumbnail image or custom image size
+ * @param string $max_width the max width this image will be shown to build the sizes attribute
+ */
+
+function awesome_acf_responsive_image($hero_carousel_image,$image_size,$max_width){
+
+	// check the image ID is not blank
+	if($hero_carousel_image != '') {
+
+		// set the default src image size
+		$image_src = wp_get_attachment_image_url( $hero_carousel_image, $image_size );
+
+		// set the srcset with various image sizes
+		$image_srcset = wp_get_attachment_image_srcset( $hero_carousel_image, $image_size );
+
+		// generate the markup for the responsive image
+		echo 'src="'.$image_src.'" srcset="'.$image_srcset.'" sizes="(max-width: '.$max_width.') 100vw, '.$max_width.'"';
+
+	}
+}
+
+?>
+  <div><img data-hero layout="intrinsic" <?php awesome_acf_responsive_image(get_field( 'image_1' ),'thumb-640','640px'); ?> alt="<?php echo $hero_carousel_image['alt']; ?>"/>
 
 	  </div>
 	  <?php endwhile; wp_reset_query(); ?>
